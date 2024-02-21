501766c890a2c246f608c6457a6665e1526aef26
Raul Bezares <rbezares5@gmail.com>
2024-02-21 19:10:05 +0900

    Revert "Add DummyClass"
    This reverts commit 3ebc52fe1683872b07b72d70fb08ed05ee73eba3.

--- lumache.py
+++ lumache.py
@@ -14,7 +14,3 @@ def get_random_ingredients(kind=None):
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
     pass
-
-class DummyClass():
-    """Testing updates to the code."""
-    pass
